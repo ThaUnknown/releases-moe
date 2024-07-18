d1f41c4bd4fd124e181466ffebb5eaa18b34e49a
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2024-07-18 21:41:53 +0200

    fix: home page load

--- sk/src/routes/+page.ts
+++
@@ -1,5 +0,0 @@
-import { client } from '$lib/pocketbase/index.js'
-
-export const load = async () => {
-  return { ids: (await client.collection('listIDs').getFullList({ batch: 10000, fields: 'alID' })).map(({ alID }) => alID) }
-}
