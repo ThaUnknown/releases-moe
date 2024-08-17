e1336c870d60b4f2a19ab75b77bec47e695e5228
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2024-08-18 00:40:10 +0200

    feat: embeds

---
+++ sk/src/hooks.server.ts
@@ -0,0 +1,85 @@
+import PocketBase from 'pocketbase'
+import { env } from '$env/dynamic/private'
+import type { EntriesResponse, TorrentsResponse } from '$lib/pocketbase/generated-types'
+
+const client = new PocketBase(env.PROXY_TARGET || 'http://127.0.0.1:59992')
+
+export const handle = async ({ event, resolve }) => {
+  if (!event.params.id || !Number(event.params.id)) return resolve(event)
+  const ua = event.request.headers.get('user-agent')
+
+  if (!ua?.includes('Discordbot')) return resolve(event)
+  try {
+    const item = await client.collection('entries').getFirstListItem<EntriesResponse<{
+      trs: TorrentsResponse[]
+    }>>(`alID="${event.params.id}"`, { expand: 'trs' })
+
+    const res = await fetch('https://graphql.anilist.co', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        query: /* js */`
+          query($id: Int) {
+            Media(id: $id) {
+              title {
+                english,
+                userPreferred
+              }
+            }
+          }
+        `,
+        variables: {
+          id: Number(event.params.id)
+        }
+      })
+    })
+    if (!res.ok && res.status === 429) {
+      throw res
+    }
+
+    const { data } = await res.json()
+
+    let desc = ''
+    const best = item.expand?.trs.filter(({ isBest }) => isBest)
+    if (best?.length) {
+      desc += `Best: ${[...new Set(best.map(({ releaseGroup }) => releaseGroup))].join('/')}\n`
+    }
+    const alt = item.expand?.trs.filter(({ isBest }) => !isBest)
+    if (alt?.length) {
+      desc += `Alt: ${[...new Set(alt.map(({ releaseGroup }) => releaseGroup))].join('/')}\n`
+    }
+
+    if (item.notes) desc += `\n${item.notes}\n`
+
+    if (item.comparison) desc += `\n${item.comparison.replaceAll(',', ' ')}\n`
+
+    return resolve(event, {
+      transformPageChunk: ({ html }) => {
+        return html.replace(
+          '<meta name="twitter:image" content="/favicon.png">',
+          `<meta name="twitter:image" content="https://img.anili.st/media/${event.params.id}"><meta name="twitter:card" content="summary_large_image">`
+        ).replace(
+          '<meta property="og:image" content="/favicon.png">',
+          `<meta property="og:image" content="https://img.anili.st/media/${event.params.id}"><meta property="og:site_name" content="SeaDex" />`
+        ).replace(
+          '<meta property="og:title" content="SeaDex">',
+          `<meta property="og:title" content="${data.Media.title.english || data.Media.title.userPreferred}">`
+        ).replace(
+          '<meta name="twitter:title" content="SeaDex">',
+          `<meta name="twitter:title" content="${data.Media.title.english || data.Media.title.userPreferred}">`
+        ).replace(
+          '<meta name="twitter:description" content="Your portal to the ultimate enthusiast releases - anime with unparalleled video, audio, and subtitle perfection, backed by in-depth comparisons.">',
+          `<meta name="twitter:description" content="${desc}">`
+        ).replace(
+          '<meta property="og:description" content="Your portal to the ultimate enthusiast releases - anime with unparalleled video, audio, and subtitle perfection, backed by in-depth comparisons.">',
+          `<meta property="og:description" content="${desc}">`
+        )
+      }
+    })
+  } catch (error) {
+    return resolve(event)
+  }
+}
