d92357021b3e5d846c5f537b5b268945e935c526
Cas_ <6506529+ThaUnknown@users.noreply.github.com>
2025-02-11 23:33:11 +0100

    Update sk/src/lib/components/ReleaseCard.svelte

--- sk/src/lib/components/ReleaseCard.svelte
+++ sk/src/lib/components/ReleaseCard.svelte
@@ -62,7 +62,7 @@
     <Card.Footer>
       <div class='grid grid-cols-2 gap-4 w-full'>
         {#each torrents as torrent}
-          <Button size='sm' variant='outline' class='px-4' href={torrent.url} target='_blank' class:hidden={PRIVATE_TRACKERS.includes(torrent.tracker)} class:pt-button={PRIVATE_TRACKERS.includes(torrent.tracker)}>
+          <Button size='sm' variant='outline' class='px-4 {PRIVATE_TRACKERS.includes(torrent.tracker) ? 'hidden pt-button' : ''}' href={torrent.url} target='_blank'>
             {#if icons[torrent.tracker]}
               <img src={icons[torrent.tracker]} alt={torrent.tracker} class='w-3 h-3 me-2' />
             {/if}
