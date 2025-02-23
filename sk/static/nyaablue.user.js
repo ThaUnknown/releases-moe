1958bdb0c63693e0fc52c52c7f73b7fdbe5c72c6
GHOSTDATA <lastreaction@protonmail.ch>
2025-02-23 09:21:29 -0700

    Use insertAdjecentElement with beforeend

--- sk/static/nyaablue.user.js
+++ sk/static/nyaablue.user.js
@@ -57,7 +57,7 @@ async function setViewBlue () {
         e.stopImmediatePropagation()
         window.open(`https://releases.moe/${info.alID}`, '_blank')?.focus()
       }
-      report?.appendChild(button)
+      report?.insertAdjacentHTML('beforeend', button)
     }
   }
 }
